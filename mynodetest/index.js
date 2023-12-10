95db93bac77a0c31a5d8a8455f9073955cc344a0
myuseremail <150845741+myuseremail@users.noreply.github.com>
2023-12-11 00:25:02 +0530

    Update index.js

--- mynodetest/index.js
+++ mynodetest/index.js
@@ -1,1 +1,13 @@
+//Simple http server
 
+const http = require('node:http');
+
+const server = http.createServer((req,res)=> {
+    res.writeHead(200, {'Content-Type':"text/plain"});
+    res.end("Hi, This is Sushil, Welcome to my World!");
+});
+
+const port = process.env.port || 3000
+server.listen(3000,()=>{
+    console.log("server listen on 3000 port.");
+});
